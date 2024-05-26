25f7f955030f7d9a758225303c496fa3e6f0e958
Victor Castro <vicastro@pdx.edu>
2024-05-26 16:38:33 -0700

    Refactoring class names

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -10,15 +10,15 @@ export default function Home() {
           displays the data using charts -
           <strong> Use the navigational bar to navigate to a page</strong>
         </p>
-        <h3 className="mb-2 font-semibold md:text-2xl">Home</h3>
-        <p className="mb-6 text-center sm:text-left md:text-lg">
+        <h3 className="h3-home">Home</h3>
+        <p className="p-home">
           Thank you for being here. This is a welcome/landing page that will
           give a general description of each of the pages you can navigate to.
           There are many components that make this a simple yet exciting
           homework. It was satisfying seeing everything put together.
         </p>
-        <h3 className="mb-2 font-semibold md:text-2xl">List</h3>
-        <p className="mb-6 text-center sm:text-left md:text-lg">
+        <h3 className="h3-home">List</h3>
+        <p className="p-home">
           The countries in South America are displayed as a card list like
           structure. It contains information about each country prioritizing the
           flags as the top for aesthetics. The keys are highlighted whereas the
@@ -26,16 +26,16 @@ export default function Home() {
           'N/A' along with a simple loading and error page, which is used for
           all pages.
         </p>
-        <h3 className="mb-2 font-semibold md:text-2xl">Population</h3>
-        <p className="mb-6 text-center sm:text-left md:text-lg">
+        <h3 className="h3-home ">Population</h3>
+        <p className="p-home">
           A bar graph that shows the population for each country in South
           America. I was unable to scale up the countries with low populations,
           even after testing the y/x axis scale. There are some cool components
           that you can add to make charts looks stylish. Tried adapting the
           styling to fit the general theme.
         </p>
-        <h3 className="mb-2 font-semibold md:text-2xl">GDP</h3>
-        <p className="mb-6 text-center sm:text-left md:text-lg">
+        <h3 className="h3-home">GDP</h3>
+        <p className="p-home">
           I decided to do a pie graph that shows the GDP for each country.
           Again, I was not able to scale the country with small a small GDP. The
           colors are randomly generated to save time.
